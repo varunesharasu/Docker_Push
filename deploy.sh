a3de3a95bcaffbff5eedd4101d3b9be845ea61e0
Varunesh T <tvarunesharasu@gmail.com>
2025-03-19 15:42:45 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # Set Docker image name, tag, and container name
 IMAGE_NAME="varunesharasu/test"
 TAG="latest"
-CONTAINER_NAME="jenkins-docker-container"
+CONTAINER_NAME="my-container"
 
 # Stop and remove any existing container with the same name
 docker stop $CONTAINER_NAME || true
